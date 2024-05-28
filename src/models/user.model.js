9eea3dfdcc4ad1a0c8c60d2dfa3e0e16935cfff5
Nishantsingh11 <ns114046@gmail.com>
2024-05-28 10:34:56 +0530

    chore: Update npm dependencies for bcrypt, jsonwebtoken, and mongoose-aggregate-paginate-v2 use pre and other method like is password correct password hash on save generateAccessToken and generateRefershToken

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,82 @@
-import mongosse,{Schema,Model} from "mongoose"
+import mongosse, { Schema, Model, model } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "Username must be unique"],
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverimage: {
+      type: String,
+    },
+    watchhistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (req, res, next) {
+  if (!this.isModified("password")) return next(); // this helps to hash the password only if the user has change the password otherwise is will call next fucntion
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+// method to compare the password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password); //help to compare the password then it return true and false
+};
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: ACCESS_TOKEN_EXPAIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN,
+    { expiresIn: REFRESH_TOKEN_EXPAIRY }
+  );
+};
+export const User = model("User", userSchema);
